ffbb70acf67c19afcd6a9b03136c8505144ea3ef
Daphne <daphne@daphne.(none)>
2013-09-12 12:26:36 -0400

    Added tiles and set width/height to the board

--- src/game.js
+++ src/game.js
@@ -1,8 +1,28 @@
 Game = {
+    // Defines size of grid and size of tiles in grid
+    map_grid: {
+        width: 24,
+        height: 16,
+        tile: {
+            width: 16,
+            height:16
+        }
+    },
+
+    // Total width of the game screen
+    width: function() {
+        return this.map_grid.width * this.map_grid.tile.width;
+    },
+
+    // Total height of the game screen
+    height: function() {
+        return this.map_grid.height * this.map_grid.tile.height;
+    },
+
     // Initialize and start game
     start: function() {
         // Start crafty and set background color
-        Crafty.init(480, 320);
+        Crafty.init(Game.width(), Game.height());
         Crafty.background('green');
     }
 }
