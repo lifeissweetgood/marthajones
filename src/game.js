49ecbe44912e24b534d1683cb8034e20934631a1
Daphne <daphne@daphne.(none)>
2013-09-12 17:57:24 -0400

    Made a game scene ocmponent.

--- src/game.js
+++ src/game.js
@@ -23,21 +23,7 @@ Game = {
     start: function() {
         // Start crafty and set background color
         Crafty.init(Game.width(), Game.height());
-        //Crafty.background('rgb(249, 223, 125)');
-
-        Crafty.e('PlayerCharacter').at(5,5);
-
-        for(var x=0; x < Game.map_grid.width; x++) {
-            for(var y=0; y < Game.map_grid.height; y++) {
-                var at_edge = x == 0 || x == Game.map_grid.width - 1 || y == 0 || y == Game.map_grid.height - 1;
-
-                if(at_edge) {
-                    Crafty.e('Tree').at(x, y);
-                } else if(Math.random() < 0.06) {
-                    Crafty.e('Human').at(x, y);
-                }
-            }
-        }
+        Crafty.background('rgb(249, 223, 125)');
 
         Crafty.scene('Loading');
     }
