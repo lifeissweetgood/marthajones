bac06e446e4a8be6e6c0dcbda322b31d329edf7a
Daphne <daphne@daphne.(none)>
2013-09-12 15:50:56 -0400

    Added a player character; still doing Crafty tutorial

--- src/game.js
+++ src/game.js
@@ -25,6 +25,8 @@ Game = {
         Crafty.init(Game.width(), Game.height());
         Crafty.background('rgb(249, 223, 125)');
 
+        Crafty.e('PlayerCharacter').at(5,5);
+
         for(var x=0; x < Game.map_grid.width; x++) {
             for(var y=0; y < Game.map_grid.height; y++) {
                 var at_edge = x == 0 || x == Game.map_grid.width - 1 || y == 0 || y == Game.map_grid.height - 1;
